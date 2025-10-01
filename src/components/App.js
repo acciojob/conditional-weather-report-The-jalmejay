c8285ca07e4693b307a7c656bfe5e80450d127fb
JALMEJAY KUMAR SHAW <116828650+The-jalmejay@users.noreply.github.com>
2025-10-02 00:01:34 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 
 import React,{useState} from "react";
 import './../styles/App.css';
-import WeatherDisplay from "./components/weatherDisplay";
+import WeatherDisplay from "./weatherDisplay";
 const App = () => {
   const [weather]=useState({
     temperature:20,
